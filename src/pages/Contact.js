bbe7e5cf4d3e89e55becb8fd19b141598ccce223
elkhattaby0 <eelkhattaby@gmail.com>
2024-09-02 16:39:52 +0100

    02-09-24 16:40

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,15 @@
 import ContactForm from "../components/Contact/ContactForm"
-
+import Footer from "../components/Footer"
+import Space from "../components/ui/Space"
 
 const Contact = () => {
     return (
         <div
-            className="w-full h-[89vh]"
+            className="w-full "
         >
-            {
-                <ContactForm />
-            }
+            {<ContactForm />}
+            <Space />
+            {<Footer />}
         </div>
     )
 }
