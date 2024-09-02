d16da9987041c2af2d97db3efdc3a7bf5e1c8f6d
elkhattaby0 <eelkhattaby@gmail.com>
2024-09-02 16:20:19 +0100

    02-09-24 16:20

--- src/components/Home/Timeline.js
+++ src/components/Home/Timeline.js
@@ -11,6 +11,32 @@ const Timeline = () => {
             >&#8729; Journey &#8729;</ParagraphFive>
 
             <div class="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
+                {/* Left */}
+                <div class="flex md:contents flex-row-reverse">
+                    <div
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                        <h3 
+                            style={{color: Palette.primaryColor}}
+                            class="text-lg font-semibold lg:text-xl"
+                        >Freelancer</h3>
+                        <span 
+                            style={{color: Palette.accentColor}}
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap"
+                        >2024 - Now</span>
+                    </div>
+                    <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                        <div class="flex items-center justify-center w-6 h-full">
+                            <div 
+                                style={{backgroundColor: Palette.HighlightColor}}
+                                class="w-1 h-full rounded-t-full">
+                            </div>
+                        </div>
+                        <div 
+                            style={{backgroundColor:Palette.backgroundColor , border:`4px solid ${Palette.HighlightColor}`}}
+                            class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                    </div>
+                </div>
             {/* Right */}
             <div class="flex md:contents">
                     <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
@@ -29,10 +55,11 @@ const Timeline = () => {
                         class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
                         <h3 
                             style={{color: Palette.primaryColor}}
-                            class="text-lg font-semibold lg:text-xl">Freelancer</h3>
+                            class="text-lg font-semibold lg:text-xl">Internship</h3>
+                            <p class="mt-2 leading-6">At Nowotch "Restaurant website"</p>
                         <span 
                             style={{color: Palette.accentColor}}
-                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2024 - Now</span>
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2024</span>
                     </div>
                 </div>
 
