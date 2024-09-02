b321ab6ec5aed0f05264b32b86da278f93da0427
elkhattaby0 <eelkhattaby@gmail.com>
2024-09-02 13:25:31 +0100

    02-09-24 13:30

--- src/components/Home/Timeline.js
+++ src/components/Home/Timeline.js
@@ -8,11 +8,39 @@ const Timeline = () => {
             <ParagraphFive 
                 uicss="text-3xl text-center my-8"
                 stl={Palette.secondaryColor}
-            >&#8729; Qualification  &#8729;</ParagraphFive>
+            >&#8729; Journey &#8729;</ParagraphFive>
+
             <div class="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
+            {/* Right */}
+            <div class="flex md:contents">
+                    <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                        <div class="flex items-center justify-center w-6 h-full">
+                            <div 
+                                style={{backgroundColor: Palette.HighlightColor}}
+                                class="w-1 h-full rounded-t-full">
+                            </div>
+                        </div>
+                        <div 
+                            style={{backgroundColor:Palette.backgroundColor , border:`4px solid ${Palette.HighlightColor}`}}
+                            class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                    </div>
+                    <div 
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
+                        <h3 
+                            style={{color: Palette.primaryColor}}
+                            class="text-lg font-semibold lg:text-xl">Freelancer</h3>
+                        <span 
+                            style={{color: Palette.accentColor}}
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2024 - Now</span>
+                    </div>
+                </div>
+
+                {/* Left */}
                 <div class="flex md:contents flex-row-reverse">
                     <div
-                        class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
                         <h3 
                             style={{color: Palette.primaryColor}}
                             class="text-lg font-semibold lg:text-xl"
@@ -21,7 +49,56 @@ const Timeline = () => {
                         <span 
                             style={{color: Palette.accentColor}}
                             class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap"
-                        >2022 - 2024</span>
+                        >2023 - 2024</span>
+                    </div>
+                    <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                        <div class="flex items-center justify-center w-6 h-full">
+                            <div 
+                                style={{backgroundColor: Palette.HighlightColor}}
+                                class="w-1 h-full rounded-t-full">
+                            </div>
+                        </div>
+                        <div 
+                            style={{backgroundColor:Palette.backgroundColor , border:`4px solid ${Palette.HighlightColor}`}}
+                            class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                    </div>
+                </div>
+                {/* Right */}
+                <div class="flex md:contents">
+                    <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
+                        <div class="flex items-center justify-center w-6 h-full">
+                            <div 
+                                style={{backgroundColor: Palette.HighlightColor}}
+                                class="w-1 h-full rounded-t-full">
+                            </div>
+                        </div>
+                        <div 
+                            style={{backgroundColor:Palette.backgroundColor , border:`4px solid ${Palette.HighlightColor}`}}
+                            class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                    </div>
+                    <div 
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
+                        <h3 
+                            style={{color: Palette.primaryColor}}
+                            class="text-lg font-semibold lg:text-xl">Macdonald</h3>
+                        <span 
+                            style={{color: Palette.accentColor}}
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2023</span>
+                    </div>
+                </div>
+
+                <div class="flex md:contents flex-row-reverse">
+                    <div
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                        <h3 
+                            style={{color: Palette.primaryColor}}
+                            class="text-lg font-semibold lg:text-xl">Digital Development</h3>
+                        <p class="mt-2 leading-6">OFPPT</p>
+                        <span 
+                            style={{color: Palette.accentColor}}
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2022 - 2023</span>
                     </div>
                     <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
                         <div class="flex items-center justify-center w-6 h-full">
@@ -38,48 +115,54 @@ const Timeline = () => {
 
                 <div class="flex md:contents">
                     <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-                        <div class="flex items-center justify-center w-6 h-full">
-                            <div class="w-1 h-full bg-indigo-300"></div>
+                            <div class="flex items-center justify-center w-6 h-full">
+                                <div 
+                                    style={{backgroundColor: Palette.HighlightColor}}
+                                    class="w-1 h-full rounded-t-full">
+                                </div>
+                            </div>
+                            <div 
+                                style={{backgroundColor:Palette.backgroundColor , border:`4px solid ${Palette.HighlightColor}`}}
+                                class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
                         </div>
-                        <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-                    </div>
-                    <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
-                        <h3 class="text-lg font-semibold lg:text-xl">chemistry</h3>
+                    <div 
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
+                        <h3 
+                            style={{color: Palette.primaryColor}}
+                            class="text-lg font-semibold lg:text-xl">Chemistry</h3>
                         <p class="mt-2 leading-6">University</p>
-                        <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2020 - 2022</span>
+                        <span 
+                            style={{color: Palette.accentColor}}
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2020 - 2022</span>
                     </div>
                 </div>
-
                 <div class="flex md:contents flex-row-reverse">
                     <div
-                        class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
-                        <h3 class="text-lg font-semibold lg:text-xl">high school diploma "BAC"</h3>
+                        style={{backgroundColor: Palette.accentColor}}
+                        class="relative p-4 my-6 rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
+                        <h3 
+                            style={{color: Palette.primaryColor}}
+                            class="text-lg font-semibold lg:text-xl"
+                        >Life and Earth Sciences</h3>
                         <p class="mt-2 leading-6">High School</p>
-                        <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">2022</span>
+                        <span 
+                            style={{color: Palette.accentColor}}
+                            class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap"
+                        >2020</span>
                     </div>
                     <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
                         <div class="flex items-center justify-center w-6 h-full">
-                            <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
+                            <div 
+                                style={{backgroundColor: Palette.HighlightColor}}
+                                class="w-1 h-full rounded-t-full">
                             </div>
                         </div>
-                        <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
+                        <div 
+                            style={{backgroundColor:Palette.backgroundColor , border:`4px solid ${Palette.HighlightColor}`}}
+                            class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
                     </div>
                 </div>
-
-                <div class="flex md:contents">
-                    <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-                        <div class="flex items-center justify-center w-6 h-full">
-                            <div class="w-1 h-full bg-indigo-300"></div>
-                        </div>
-                        <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-                    </div>
-                    <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
-                        <h3 class="text-lg font-semibold lg:text-xl">New Event 4</h3>
-                        <p class="mt-2 leading-6">Description of the second event.</p>
-                        <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 4</span>
-                    </div>
-                </div>
-
             </div>
             </div>
         </div>
