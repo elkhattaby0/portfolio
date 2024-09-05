c155e40f29fd1397aa3be67632eaba992ab30922
elkhattaby0 <eelkhattaby@gmail.com>
2024-09-06 00:44:09 +0100

    05-09-24 00:45

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
     return (
         <div className={uicss.fullcontainer} style={{ backgroundColor: Palette.backgroundColor }}>
             <section
-                className={`fixed top-0 z-10 w-full  flex justify-center ${isVisible >10 ? " shadow-md border-b" : ""}`}
+                className={`fixed top-0 z-10 w-full  flex justify-center ${isVisible >10 ? "border-b" : ""}`}
                 style={isVisible ? 
                     { backgroundColor: Palette.backgroundColor } : 
                     {backgroundColor: "none"}
