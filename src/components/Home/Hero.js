ba2426103e2c5f96267bee1528b548ea0455c0bb
elkhattaby0 <eelkhattaby@gmail.com>
2024-09-08 17:55:38 +0100

    08-09-24 17:55

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -6,37 +6,34 @@ import { Typewriter } from 'react-simple-typewriter'
 
 const Hero = ({currentLang}) => {
     const uicss = {
-        "container":"h-screen w-10/12 flex max-md:flex-col max-md:flex-col-reverse justify-between items-center",
-        "para": "self-start",
+        "container":"h-screen w-10/12 flex max-md:flex-col max-md:flex-col-reverse justify-between max-md:justify-center items-center",
+        "para": "self-start my-2",
     }
     return (
         <section className={uicss.container} style={{backgroundColor: Palette.backgroundColor}}>
             <div 
-                className="flex flex-col text-left justify-center w-1/2 max-md:w-full h-full"
+                className="flex flex-col text-left justify-center w-1/2 max-md:w-full h-full max-md:h-fit"
             >
                 <h1 className={`font-bold text-6xl max-md:text-4xl w-full ${uicss.para}`} style={{color:Palette.HighlightColor}}>
                     {currentLang.headtitle}
                 </h1>
-                <br />
                 <h3 className={`text-4xl max-md:text-2xl w-full ${uicss.para}`} style={{color:Palette.primaryColor}}>
                     <Typewriter
                         words={currentLang.specialty}
                         loop="false"
                     />
                 </h3>
-                <br />
                 <p className={`text-xl w-full ${uicss.para}`} style={{color:Palette.secondaryColor}}>{currentLang.text}</p>
-                <br />
                 <Button 
                     link={currentLang.btn.slug} uicss="self-start"
                     stl={Palette.backgroundColor}    
                 >{currentLang.btn.text}</Button>
             </div>
             <div
-                className="w-1/2 max-md:w-full h-fit flex items-center justify-end max-md:justify-center max-md:items-end"
+                className="w-1/2 h-full max-md:w-full h-full flex items-center justify-end max-md:justify-center max-md:items-end "
             >
                 <div 
-                    className="relative w-96 h-[70%] max-md:w-60 max-md:h-60 rounded-full overflow-hidden border"
+                    className="relative w-96 h-[70%] max-md:w-60 max-md:h-[70%] rounded-full overflow-hidden  border"
                     style={{backgroundColor: Palette.HighlightColor}}
                 >
                     <Image
