a4cf00454acb98ddecadc0adc202cab21b94fe16
elkhattaby0 <eelkhattaby@gmail.com>
2024-09-08 19:09:53 +0100

    08-09-24 19:10

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -22,10 +22,10 @@ const Hero = ({currentLang}) => {
                         words={currentLang.specialty}
                         loop="false"
                     />
-                </h3>
+                </h3>                
                 <p className={`text-xl w-full ${uicss.para}`} style={{color:Palette.secondaryColor}}>{currentLang.text}</p>
                 <Button 
-                    link={currentLang.btn.slug} uicss="self-start"
+                    link={currentLang.btn.slug} uicss="self-start my-2"
                     stl={Palette.backgroundColor}    
                 >{currentLang.btn.text}</Button>
             </div>
@@ -33,7 +33,7 @@ const Hero = ({currentLang}) => {
                 className="w-1/2 h-full max-md:w-full max-md:h-fit flex items-center justify-end max-md:justify-center max-md:items-end"
             >
                 <div 
-                    className="relative w-96 h-[70%] max-md:w-60 max-md:mb-2 max-md:h-60 rounded-full overflow-hidden border "
+                    className="relative w-96 h-[70%] max-md:w-60 max-md:mb-2 max-md:h-80 rounded-full overflow-hidden border"
                     style={{backgroundColor: Palette.HighlightColor}}
                 >
                     <Image
