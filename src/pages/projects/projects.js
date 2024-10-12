c457cfa95ea3553b90d4cad85ce53043d45828a8
elkhattaby0 <eelkhattaby@gmail.com>
2024-10-12 21:54:07 +0100

    12-10-24 21:55

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,21 +1,17 @@
 import "./projects.css"
 
 const proj = [
-    // {
-    //     "title": "Daily Prayer Time",
-    //     "img": "../../assets/project1.webp",
-    //     "tech": ["Python", "Flask"]
-    // },
-    // {
-    //     "title": "Qneksi Blog",
-    //     "img": "../../assets/project1.webp",
-    //     "tech": ["Next js", "Sass"]
-    // },
-    // {
-    //     "title": "Opiniometer",
-    //     "img": "../../assets/project1.webp",
-    //     "tech": ["React Js", "Python", "Chart Js"]
-    // },
+    {
+        "title": "Dolce Production",
+        "link": "https://dolceproduction.com/",
+        "tech": ["Wordpress", "Elementor"]
+    },
+    {
+        "title": "Atlas Azawan",
+        "link": "https://atlasazawan.com/",
+        "tech": ["Wordpress", "Elementor"]
+    },
+    
 ]
 
 const Projects = () => {
@@ -25,7 +21,12 @@ const Projects = () => {
             {
                 proj.map(n=> (
                     <div className="projectCard">
-                        <img src={require('../../assets/project1.webp')} />   
+                        <iframe 
+                            src={n.link} 
+                            title={n.title}
+                            width="100%"    
+                            height="300px"
+                        />   
                         <span>
                             <h4>{n.title}</h4>
                             <a>&#9626;</a>
