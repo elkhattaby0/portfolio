2fc9a1f386471c861194745bd6e2596432332dac
elkhattaby0 <eelkhattaby@gmail.com>
2024-10-14 21:55:29 +0100

    14-10-24 21:55

--- src/pages/hero/hero.js
+++ src/pages/hero/hero.js
@@ -1,10 +1,48 @@
 import "./hero.css";
 
+import { SiGithub, SiInstagram, SiLinkedin, SiWhatsapp } from "react-icons/si";
+const sizeIcon = 25;
+const colorIcon = "#EFFFFB"
+const social = [
+    {
+        "img": <SiGithub size={sizeIcon} color={colorIcon} />,
+        "link": "https://github.com/elkhattaby0"
+    },
+    {
+        "img": <SiInstagram size={sizeIcon} color={colorIcon} />,
+        "link": "https://www.instagram.com/lahoucine_elkhattaby"
+    },
+    {
+        "img": <SiLinkedin size={sizeIcon} color={colorIcon} />,
+        "link": "https://www.linkedin.com/in/lahoucine-el-khattaby-0335341bb"
+    },
+    {
+        "img": <SiWhatsapp size={sizeIcon} color={colorIcon} />,
+        "link": "https://wa.me/212650999953"
+    },
+]
+
 const Hero = () => {
     return <div className="hero" id="about">
         <h1>&#10024; Introduction.</h1>
-        <p>Hi, I'm Lahoucine EL KHATTABY. I'm a Junior <span>Full Stack Developer</span>. I enjoy building websites and learning new things. I've been working with <span>WordPress</span>, and I like creating user friendly sites. I'm excited to keep improving my skills and see where this journey takes me. 
-        </p>
+        <h2><span>Hi,</span>
+            <br/>
+            I'm Lahoucine.</h2>
+        <p>
+            I'm a Junior <span>Full Stack Developer</span> skilled in front-end and back-end development. I want to learn and improve my skills. I enjoy working on projects that challenge me and help me grow in my career.
+        </p><br/>
+        <div>
+            {
+                social.map(n=>(
+                    <a 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={n.link}>
+                        {n.img}
+                    </a>     
+                ))
+            }
+        </div>
     </div>
 }
 
--- src/pages/skills/skills.js
+++ src/pages/skills/skills.js
@@ -1,128 +1,133 @@
 import React, { useState } from "react";
-import { SiHtml5, SiCss3, SiBootstrap, SiTailwindcss, SiJquery, SiJavascript, SiPython, SiMysql, SiMongodb, SiNodedotjs, SiPhp, SiLaravel, SiReact, SiTypescript, SiWordpress, SiExpress, SiGit, SiGithub, SiUml, SiVisualstudiocode, SiElementor, SiCanva, SiJson, SiRabbitmq } from "react-icons/si"; 
+import { SiHtml5, SiCss3, SiBootstrap, SiTailwindcss, SiJquery, SiJavascript, SiPython, SiMysql, SiMongodb, SiNodedotjs, SiPhp, SiLaravel, SiReact, SiTypescript, SiWordpress, SiExpress, SiGit, SiGithub, SiUml, SiVisualstudiocode, SiElementor, SiCanva, SiJson, SiRabbitmq, SiPostman } from "react-icons/si"; 
 
 import "./skills.css";
-
+const sizeIcon = 40; 
 const proj = [
     {
         name: "HTML",
-        icon: <SiHtml5 size={45} color="#EFFFFB" />,
+        icon: <SiHtml5 size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "CSS",
-        icon: <SiCss3 size={45} color="#EFFFFB" />,
+        icon: <SiCss3 size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "Bootstrap",
-        icon: <SiBootstrap size={45} color="#EFFFFB" />,
+        icon: <SiBootstrap size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "Tailwind",
-        icon: <SiTailwindcss size={45} color="#EFFFFB" />,
+        icon: <SiTailwindcss size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "JQuery",
-        icon: <SiJquery size={45} color="#EFFFFB" />,
+        icon: <SiJquery size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "JavaScript",
-        icon: <SiJavascript size={45} color="#EFFFFB" />,
+        icon: <SiJavascript size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "Python",
-        icon: <SiPython size={50} color="#EFFFFB" />,
+        icon: <SiPython size={sizeIcon} color="#EFFFFB" />,
         category: "Backend",
     },
     {
         name: "MySQL",
-        icon: <SiMysql size={45} color="#EFFFFB" />,
+        icon: <SiMysql size={sizeIcon} color="#EFFFFB" />,
         category: "Database",
         
     },
     {
         name: "MongoDB",
-        icon: <SiMongodb size={45} color="#EFFFFB" />,
+        icon: <SiMongodb size={sizeIcon} color="#EFFFFB" />,
         category: "Database",
     },
     {
         name: "NodeJs",
-        icon: <SiNodedotjs size={45} color="#EFFFFB" />,
+        icon: <SiNodedotjs size={sizeIcon} color="#EFFFFB" />,
         category: "Backend",
     },
     {
         name: "PHP",
-        icon: <SiPhp size={45} color="#EFFFFB" />,
+        icon: <SiPhp size={sizeIcon} color="#EFFFFB" />,
         category: "Backend",
     },
     {
         name: "Laravel",
-        icon: <SiLaravel size={45} color="#EFFFFB" />,
+        icon: <SiLaravel size={sizeIcon} color="#EFFFFB" />,
         category: "Backend",
     },
     {
         name: "ReactJs",
-        icon: <SiReact size={45} color="#EFFFFB" />,
+        icon: <SiReact size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "TypeScript",
-        icon: <SiTypescript size={45} color="#EFFFFB" />,
+        icon: <SiTypescript size={sizeIcon} color="#EFFFFB" />,
         category: "Frontend",
     },
     {
         name: "Wordpress",
-        icon: <SiWordpress size={45} color="#EFFFFB" />,
+        icon: <SiWordpress size={sizeIcon} color="#EFFFFB" />,
         category: "CMS",
     },
     {
         name: "ExpressJs",
-        icon: <SiExpress size={45} color="#EFFFFB" />,
+        icon: <SiExpress size={sizeIcon} color="#EFFFFB" />,
+        category: "Others",
+    },
+    {
+        name: "Postman",
+        icon: <SiPostman size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "Git",
-        icon: <SiGit size={45} color="#EFFFFB" />,
+        icon: <SiGit size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "GitHub",
-        icon: <SiGithub size={45} color="#EFFFFB" />,
+        icon: <SiGithub size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "UML",
-        icon: <SiUml size={45} color="#EFFFFB" />,
+        icon: <SiUml size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "VSCode",
-        icon: <SiVisualstudiocode size={45} color="#EFFFFB" />,
+        icon: <SiVisualstudiocode size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "Elementor",
-        icon: <SiElementor size={45} color="#EFFFFB" />,
+        icon: <SiElementor size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "Canva",
-        icon: <SiCanva size={45} color="#EFFFFB" />,
+        icon: <SiCanva size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "JSON",
-        icon: <SiJson size={45} color="#EFFFFB" />,
+        icon: <SiJson size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     {
         name: "RabbitMQ",
-        icon: <SiRabbitmq size={45} color="#EFFFFB" />,
+        icon: <SiRabbitmq size={sizeIcon} color="#EFFFFB" />,
         category: "Others",
     },
     
